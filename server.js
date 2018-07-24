764a8b1c44c502202c86959effa444305946ff78
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-24 09:30:33 -0700

    Add necessary files and remove old ones

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 /* eslint no-console: off */
-const restify = require('restify');
+require('dotenv').config();
+const { createServer } = require('http');
+require('./lib/mongodb');
+const app = require('./lib/app');
 
-function respond(req, res, next) {
-    res.send('hello ' + req.params.name);
-    next();
-}
+const PORT = process.env.PORT || 3000;
+const server = createServer(app);
 
-const server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
-
-server.listen(8080, () => {
-    console.log('%s listening at %s', server.name, server.url);
-});
+server.listen(PORT, () => console.log('server running on', server.address().port));
